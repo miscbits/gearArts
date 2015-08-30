4ddfeda0685eebd8f08af11d2c5974ac6e8e2cc1
miscbits <willow6786@gmail.com>
2015-08-30 19:04:58 -0400

    added create app method to app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, make_response, json
 
-app = create_app()
+def create_app():
+	app = Flask(__name__)
+	app.secret_key = "lL882eTTDIetmdJGDuQhUnurAee6GNCTfqw2CjtWolKRH8uh9oct8qOG9veFr7kq5UzuGKRdCig3DhP4fuGRmhZQgUwIu2LzeHMSzLxKknlZMreiKLri696pqmjCxv8tdJJoI23xQUIGmFgOSnFVf1REirCRZGxXg7AGCTnWRghbHlDCPyFxAQGZdwl5dlkFVhB1C98rh9Rm1PjECyq5jJybt7SyZT47XQKRE1Wr0h1jmoSFoKwc2OdcL6d5bn5uTuTnLyeu77IMHvKwDIaFLed3Uy8sSO5E8Zix99WaE9VS"
+	return app
 
+app = create_app()
 
 @app.route('/')
 def index():
